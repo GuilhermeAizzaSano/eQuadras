9df442e8e56eed12f60f169ae4ba071783f80a4d
Guilherme_Aizza_Sano <guilhermeaizzasanox@gmail.com>
2026-09-02 13:46:20 -0300

    fix(openapi): remove name property from HTTP Bearer SecurityScheme for strict OpenAPI validation

--- src/main/java/com/agendamentos/equadras/config/OpenApiConfig.java
+++ src/main/java/com/agendamentos/equadras/config/OpenApiConfig.java
@@ -33,7 +33,6 @@ public class OpenApiConfig {
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEME_NAME,
                                 new SecurityScheme()
-                                        .name(SECURITY_SCHEME_NAME)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
